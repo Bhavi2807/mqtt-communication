70288e84e2222d53de62257f6050e4545f191e0e
Bhavi2807 <109072332+Bhavi2807@users.noreply.github.com>
2022-07-20 15:04:52 +0530

    Add files via upload

---
+++ schedule.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+// #include <time.h>
+#include<chrono>
+#include<thread>
+using namespace std;
+class schedule{
+    public:
+    void time()
+    {
+        this_thread::sleep_for(chrono::milliseconds(30000));
+    }
+};
+int main(){
+    schedule SC;
+    cout<<"This is a sleep_for demonstration\n";
+    SC.time();
+    cout<<"I slept for 60000 milliseconds\n";
+    return 0;
+}
+
